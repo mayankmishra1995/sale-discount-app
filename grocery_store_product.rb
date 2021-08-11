448a09e832310c399886f89a05f3d57198f3f117
mm884209@gmail.com <mm884209@gmail.com>
2021-08-11 16:18:53 +0530

    calculation of product price.

--- grocery_store_product.rb
+++ grocery_store_product.rb
@@ -27,5 +27,27 @@ class GroceryStoreProduct
       }
     }
 
+    @product_name = product_name
+    @product_quantity = product_quantity
+    @product_price_after_discount = calculate_product_price_after_discount
+    @product_price_before_discount = calculate_product_price_before_discount  
   end
+
+  def calculate_product_price_after_discount
+    case @product_name
+    when "milk"
+      sale_on_quantity = 2
+      final_milk_price = discount_calculator(@products_details[@product_name]["unit_price"], @products_details[@product_name]["sale_unit_price"],sale_on_quantity)
+      return final_milk_price 
+    when "bread"
+      sale_on_quantity = 3
+      final_bread_price = discount_calculator(@products_details[@product_name]["unit_price"], @products_details[@product_name]["sale_unit_price"],sale_on_quantity)
+      return final_bread_price
+    when "banana"
+      return @products_details[@product_name]["unit_price"] * @product_quantity
+    when "apple"
+      return @products_details[@product_name]["unit_price"] * @product_quantity
+    end
+  end
+
 end
